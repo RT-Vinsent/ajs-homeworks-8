d50c32856f4bde620e8a5f9d9419ca333adda5f4
Roman T <rt.hagbo@gmail.com>
2022-03-21 09:08:11 +0500

    Дз №8 «Контейнеры» - Set

---
+++ src/js/app.js
@@ -0,0 +1,41 @@
+/* eslint-disable no-console */
+import Team from './modules/team';
+
+const alex = {
+  name: 'ALex',
+  type: 'Zombie',
+  attack: 40,
+  defence: 10,
+  health: 100,
+  level: 1,
+};
+
+const vova = {
+  name: 'Vova',
+  type: 'Magician',
+  attack: 10,
+  defence: 40,
+  health: 100,
+  level: 1,
+};
+
+const dima = {
+  name: 'Dima',
+  type: 'Daemon',
+  attack: 10,
+  defence: 40,
+  health: 100,
+  level: 1,
+};
+
+const team = new Team();
+team.addAll(alex, vova);
+team.addAll(alex, vova);
+team.addAll(alex);
+team.add(vova);
+team.add(dima);
+
+team.toArray();
+
+console.log(team.membersArr);
+team.members.forEach((size) => console.log(size));
